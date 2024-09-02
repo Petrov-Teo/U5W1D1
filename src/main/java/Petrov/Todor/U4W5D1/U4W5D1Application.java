4a2b5251394689da305966a1aae9e58039e5e4f6
Petrov-Teo <168548498+Petrov-Teo@users.noreply.github.com>
2024-09-02 14:01:41 +0200

    Initial commit

---
+++ src/main/java/Petrov/Todor/U4W5D1/U4W5D1Application.java
@@ -0,0 +1,17 @@
+package Petrov.Todor.U4W5D1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U4W5D1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(U4W5D1Application.class, args);
+        System.out.println("________________________________________");
+        System.out.println("Hello Word");
+        System.out.println("________________________________________");
+    }
+
+
+}
---
+++ src/test/java/Petrov/Todor/U4W5D1/U4W5D1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Petrov.Todor.U4W5D1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U4W5D1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
